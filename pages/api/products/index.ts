0d2a98e5fc698f7d7b6910047ba19eb5d5bd30ed
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-10-09 21:25:55 -0300

    fix limit and offset

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,11 +3,19 @@ import { products } from "../../../lib/algolia";
 import methods from "micro-method-router";
 
 function getLimitAndOffset(req: NextApiRequest, maxLimit, totalItems) {
+	var limit = 10;
+	var offset = 0;
+
 	const queryLimit = parseInt(req.query.limit as string);
 	const queryOffset = parseInt(req.query.offset as string);
 
-	const limit = queryLimit <= maxLimit ? queryLimit : maxLimit;
-	const offset = queryOffset < totalItems ? queryOffset : 0;
+	limit =
+		queryLimit != undefined
+			? queryLimit <= maxLimit
+				? queryLimit
+				: maxLimit
+			: limit;
+	offset = queryOffset < totalItems ? queryOffset : 0;
 
 	return {
 		limit,
@@ -20,11 +28,14 @@ export default methods({
 		const query = req.query.q;
 		const queryOffset = req.query.offset;
 		const totalHits = await (await products.search("", {})).nbHits;
+		console.log(process.env);
+		console.log(process.env);
 		const results = await products.search(query as string, {
 			hitsPerPage: parseInt(req.query.limit as string),
-			// offset: parseInt(queryOffset as string),
+			offset: parseInt(queryOffset as string),
 		});
 		const { limit, offset } = getLimitAndOffset(req, 10, totalHits);
+		// console.log(limit, offset);
 
 		res.send({
 			results: results.hits,
